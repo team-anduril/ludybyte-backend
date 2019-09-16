9b2b92a8dfd556be777548b30d123a2b63730f2e
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-09-17 00:27:55 +0100

    added documentation

---
+++ README.md
@@ -0,0 +1,48 @@
+##This to the NodeJS API (without frameworks) for anduril web app
+
+Only login and signup operations are allowed at the moment.
+Requests for login and signup, respectively are expected to be in the form: 
+
+#fetch("localhost:3000/login?email=ab@cd.com&password=123abc")
+
+AND
+
+#fetch("localhost:3000/signup?name=Johnson&email=ab@cd.com&password=123abc")
+
+Responses for succesful login or signup will be in this format:
+
+{
+	data:
+		{
+			"name": "Johnson",
+			"email": "abc.xyz.com",
+			"password": "abc123"
+		}
+} 
+
+Response after unsuccessful operations will be in this format:
+
+{
+	data:
+		{
+			"code": NUMBER,
+		}
+}
+
+
+Where NUMBER could be aby of the following:
+
+    400 - Querystring data needed for signup/login not supplied
+
+    496 - Email used for login does not exist in databse
+
+    419 - Wrong password used for login
+
+    427 - Email used for signup already is signed up 
+
+
+
+
+				© 2019, THE ANDURIL TEAM
+
+
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 ///
-/// NodeJS without ExpressJS framework
+/// NodeJS API without frameworks for anduril web app
+/// Requests for login and signup, respectively are expected to be in the form: 
+/// 	fetch("localhost:3000/login?email=ab@cd.com&password=123abc")   AND
+///		fetch("localhost:3000/signup?name=Johnson&email=ab@cd.com&password=123abc")
 ///
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-const qs = require('querystring');
+const qs = require('querystring');	// For parsing the query string part of the url. 
+									// That is where login/signup data will come from
 
-const  hostname = "https://git.heroku.com/sleepy-woodland-98503.git";
+
+const  hostname = "localhost/";
+const port = process.env.PORT || 3000; // 3000 for localhost process.env.PORT for heroku hosting
 const usersPath = "./users.json";
-const APP_SECRET = "Not you business, no?";
-let users = [];
+const APP_SECRET = "Not you business, no?"; // Used as part of the hash generation: Security measure
+let users = [];				// Booted from users.json file at start
 let loggedInUser;
 
+// For hashing of password before saving to file: Security measure. 
+// Also for generating login token for Authorization header
+//
 const performHashing  = str =>
 {
   var hash = 0, i, chr;
@@ -25,6 +34,8 @@ const performHashing  = str =>
   return hash;
 };
 
+/// Boot database file into the `users` variable at start
+///
 fs.readFile(usersPath, (err, content) =>
 {
     if (err)
@@ -37,6 +48,8 @@ fs.readFile(usersPath, (err, content) =>
     }
 });
 
+// Handle login, providing for cases like emails that have not 
+// been signed up and incorrect passwords. Receives email and password
 const login = (res, urlQueryObj) => 
 {
 	res.setHeader('Content-Type', 'application/json');
@@ -66,6 +79,8 @@ const login = (res, urlQueryObj) =>
 	res.end(JSON.stringify(users.find(a => a.email === urlQueryObj.email)));
 }
 
+// Handle signup providing for cases like already used emails
+// Receives email, name and password
 const signup = (res, urlQueryObj) => 
 {
 	res.setHeader('Content-Type', 'application/json');
@@ -95,6 +110,7 @@ const signup = (res, urlQueryObj) =>
 	res.end(JSON.stringify(users.find(a => a.email === urlQueryObj.email)));
 }
 
+// For saving users variable to json file after succesful signup
 const saveFile = user =>
 {
 	fs.writeFile(usersPath, JSON.stringify(users), err => 
@@ -108,11 +124,64 @@ const saveFile = user =>
 	});
 }
 
+// Creates server instance with basic routing
+// for accepted paths
 const server = http.createServer((req, res) => 
 {
 	let urlParts = url.parse(req.url, true);
 	let urlPathname = urlParts.pathname;
 	let urlQueryObj = urlParts.query;
+
+	if (urlPathname === "/" && (!urlQueryObj.email || !urlQueryObj.password))
+	{
+		res.setHeader('Content-Type', 'text/html');
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.statusCode = 200;
+		res.end(`
+			<div style="text-align: center; font-family: 'segoe UI', sans-serif; background: lightgray; min-height: 100vh">
+				<h1>Hello!</h1>
+				<h3>Welcome to the NodeJS API (without frameworks) for anduril web app </h3>
+				<br />
+				<p style="display: block; font-size: 18px; margin: auto; width: 60vw; text-align: left;"> 
+					Only login and signup operations are allowed at the moment. <br /><br />
+					Requests for login and signup, respectively are expected to be in the form: <br /><br /><br />
+					<code>fetch("localhost:3000/login?email=ab@cd.com&password=123abc")</code>   <br /><br /> 
+					AND <br /><br />
+					<code>fetch("localhost:3000/signup?name=Johnson&email=ab@cd.com&password=123abc")</code>
+
+					<br /><br /><br />
+					Responses for succesful login or signup will be in this format: <br /><br />
+	
+					<code>{<br />
+							data: <br /> 
+							{ <br />
+								"name": "Johnson", <br />
+								"email": "abc.xyz.com", <br />
+								"password": "abc123" <br />
+							} <br />
+					</code> } <br />
+
+					<br /><br /><br />
+					Response after unsuccessful operations will be in this format: <br /><br />
+					<code>{<br />
+							data: <br /> 
+							{ <br />
+								"code": NUMBER, <br >
+							} <br />
+					} <br /></code> <br />
+					Where NUMBER could be aby of the following:<br />
+					<ul style="text-align: left; margin-left: 300px;"><li>400 - Querystring data needed for signup/login not supplied</li><br />
+						<li>496 - Email used for login does not exist in databse </li><br />
+						<li>419 - Wrong password used for login </li><br />
+						<li>427 - Email used for signup already is signed up </li><br />
+					</ul>
+				</p>
+				<br /><br /><br /><br /><strong>&copy 2019, </strong>THE ANDURIL TEAM<br /><br />
+			</div>
+			`);
+		return;
+	}
+
 	switch(urlPathname)
 	{
 		case "/": login(res, urlQueryObj); return;
@@ -123,7 +192,7 @@ const server = http.createServer((req, res) =>
 });
 
 //const PORT = process.env.PORT
-server.listen(process.env.PORT || 3000, () => 
+server.listen(port, () => 
 {
-	console.log(`Server running at http://${hostname}/`)
+	console.log(`Server running at http://${hostname}:${port}/`)
 })
